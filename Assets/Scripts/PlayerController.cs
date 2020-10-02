6a386c2321b5db6abf494e38676a41de28b703ed
sifat <sifatraihan8@gmail.com>
2020-10-03 01:32:33 +0600

    Create Scenery, Add Fantasy skybox, Animate player,Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool jump = false;
+    public bool slide = false;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate (0,0,0.1f);
+
+        if (Input.GetKey("a"))
+        {
+            jump = true;
+        }
+
+        else {
+            jump = false;
+
+        }
+
+        if (Input.GetKey("s"))
+        {
+            slide = true;
+        }
+
+        else
+        {
+            slide = false;
+
+        }
+
+
+        if (jump == true)
+        {
+            anim.SetBool("isJump", jump);
+            transform.Translate(0, 0.4f, 0.1f);
+        
+        }
+        else if (jump == false)
+        {
+            anim.SetBool("isJump", jump);
+           
+        }
+
+        if (slide == true)
+        {
+            anim.SetBool("isSlide", slide);
+            transform.Translate(0, 0, 0.1f);
+
+        }
+        else if (slide == false)
+        {
+            anim.SetBool("isSlide", slide);
+
+        }
+    }
+}
